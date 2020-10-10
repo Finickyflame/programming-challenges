748e6e03c8df29924089b49e941f057e65b98e6c
Nicholas Berube <berube.nicholas@gmail.com>
2020-10-10 14:24:12 -0400

    Added more tests

--- src/Challenge-01/NameGenerator/RandomExtensions.cs
+++ src/Challenge-01/NameGenerator/RandomExtensions.cs
@@ -4,7 +4,11 @@ namespace NameGenerator
 {
     public static class RandomExtensions
     {
-        public static T Pick<T>(this Random random, params T[] items) => items[random.Next(items.Length)];
-        public static bool NextBool(this Random random) => random.Next(2) == 0;
+        public static T Pick<T>(this Random random, params T[] items)
+        {
+            return items[random.Next(items.Length)];
+        }
+
+        public static bool NextBool(this Random random) => random.Next(2) == 1;
     }
 }
